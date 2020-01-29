76e48c5e0acd2f53f66a9a199aefa3ce76294c27
Halconn <alexey.erpelev@outlook.com>
2020-01-29 23:49:47 +0300

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sqlite3
+import Adafruit_DHT
+import time
+from datetime import datetime, date
+
+conn = sqlite3.connect("meteodatabase.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE if not exists data_meteo (humi text, temp text, date_time text)")
+
+def get_data():
+    humidity,temperature = Adafruit_DHT.read_retry(11, 4)
+    date_time = datetime.now()
+    cursor.execute("""INSERT INTO data_meteo VALUES (?, ?, ?)""", (humidity, temperature, date_time))
+    conn.commit()
+    print(humidity,temperature, date_time)
+    print("get_data: OK")
+
+while(True):
+    get_data()
+    time.sleep(5)
